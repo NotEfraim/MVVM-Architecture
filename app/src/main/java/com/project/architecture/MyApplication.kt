550881e747f329f6fb79bd8d12fa4f1116c7e65e
Kirito <efraimcanilang@gmail.com>
2023-07-21 16:40:49 +0800

    Add Lifecycle event observer

--- app/src/main/java/com/project/architecture/MyApplication.kt
+++ app/src/main/java/com/project/architecture/MyApplication.kt
@@ -1,8 +1,34 @@
 package com.project.architecture
 
 import android.app.Application
+import android.util.Log
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.ProcessLifecycleOwner
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MyApplication : Application() {
+class MyApplication : Application(), LifecycleObserver {
+
+    private val lifeCycleEventObserver = LifecycleEventObserver{ source, event ->
+        when(event){
+            Lifecycle.Event.ON_START -> {
+
+            }
+            Lifecycle.Event.ON_CREATE -> {
+
+            }
+            Lifecycle.Event.ON_DESTROY-> {
+
+            }
+            else -> {}
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.v("CurrentActivity", this::class.java.name)
+        ProcessLifecycleOwner.get().lifecycle.addObserver(lifeCycleEventObserver)
+    }
 }
